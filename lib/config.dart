a23e97e8966dbff91f022afef2246339a2f78e2a
DrakeSr001 <magedalicf@gmail.com>
2025-11-11 17:41:44 +0200

    Update V3.5

--- lib/config.dart
+++ lib/config.dart
@@ -1,7 +1,7 @@
-const String backendBaseUrl = 'https://physical-therapy-api-cg1z.onrender.com';
+const String backendBaseUrl = 'https://physical-therapy-api.fly.dev';
 const String webAppBaseUrl = 'https://physical-therapy-qr-code.netlify.app';
 const String androidUpdateManifestUrl =
-    'https://physical-therapy-api-cg1z.onrender.com/app/releases/android';
+    'https://physical-therapy-api.fly.dev/app/releases/android';
 const String arabicAppTitle =
     '\u0645\u0631\u0643\u0632\u0020\u0627\u0644\u0639\u0644\u0627\u062C\u0020\u0627\u0644\u0637\u0628\u064A\u0639\u064A\u0020\u002D\u0020\u0641\u0631\u0639\u0020\u0627\u0644\u0648\u0641\u0627\u0621\u0020\u0648\u0627\u0644\u0623\u0645\u0644';
 const String welcome =
